15269d5d0ffcc44e852f3baadf5ab078cfe90731
Michael Guedko <mjgeko@gmail.com>
2023-03-29 19:09:30 -0400

    Add packages and 404 handle

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+require('dotenv').config()
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+//Use react for views
 app.use(express.static(path.join(__dirname, "client", "dist")));
 
 app.get("/", (req, res) => {
@@ -18,3 +20,20 @@ app.listen(PORT, () => {
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
   });
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    next(createError(404));
+  });
+  
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+module.exports = app;
