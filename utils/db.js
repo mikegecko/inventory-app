7767a5562845e722ace87defbe8272529d9b0bf4
Michael Guedko <mjgeko@gmail.com>
2023-03-30 16:45:19 -0400

    Add env for username

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.set("strictQuery", false);
 
-const mongoDB =`mongodb+srv://mjgeko:${process.env.MONGODB_KEY}@cluster0.ftvxotu.mongodb.net/?retryWrites=true&w=majority`
+const mongoDB =`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_KEY}@cluster0.ftvxotu.mongodb.net/?retryWrites=true&w=majority`
 
 main().catch((err) => console.log(err));
 async function main() {
