86ec327722cb45b7f1ed20356e2332e111135a07
Michael Guedko <mjgeko@gmail.com>
2023-03-30 16:27:57 -0400

    Add item mapping

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,44 +1,41 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 import axios from "axios";
 
 function App() {
   const [inventoryItems, setInventoryItems] = useState([]);
-  
+
   useEffect(() => {
-    axios.get('/api/inventory').then((res) => {
-      setInventoryItems(res.data);
-    }).catch((err) => {
-      console.log(err);
-    })
-  },[])
+    axios
+      .get("/api/inventory")
+      .then((res) => {
+        console.log('log' + res.data);
+        setInventoryItems(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+  }, []);
   useEffect(() => {
     console.log(inventoryItems);
-  }, [inventoryItems])
+  }, [inventoryItems]);
 
-  const displayInventory = () => {
-    if(inventoryItems.length == 0){
-      return(<li>No Items in Inventory</li>)
-    }
-    else{
-      return(inventoryItems.map((item) => (
-        <li key={item._id}>{item.title}</li>
-      )))
-    }
-  }
 
   return (
     <div className="App">
       <h1>Inventory</h1>
       <div>
         <ul>
-          {() => displayInventory()}
+          {!inventoryItems ? <li>No Items Found</li> : inventoryItems.map((item) => (
+            <li key={item._id}>{item.title}</li>
+          ))}
         </ul>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
