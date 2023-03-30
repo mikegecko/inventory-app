e04ba0b47a62028c4a073d0d736d8b0778eb9490
Michael Guedko <mjgeko@gmail.com>
2023-03-30 15:16:49 -0400

    Add basic api test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inventoryItems, setInventoryItems] = useState([]);
   
+  useEffect(() => {
+    axios.get('/api/inventory').then((res) => {
+      setInventoryItems(res.data);
+    }).catch((err) => {
+      console.log(err);
+    })
+
+  },[])
+
+
   return (
     <div className="App">
+      <h1>Inventory</h1>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ul>
+          {inventoryItems.map((item) => (
+            <li key={item._id}>{item.title}</li>
+          ))}
+        </ul>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
