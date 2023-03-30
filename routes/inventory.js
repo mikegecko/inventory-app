f6e8c0b63a44b3f4b2770fd9938c53db72d6dbe9
Michael Guedko <mjgeko@gmail.com>
2023-03-30 16:28:30 -0400

    Fix route paths

--- routes/inventory.js
+++ routes/inventory.js
@@ -5,18 +5,18 @@ const router = express.Router();
 const inventory_controller = require('../controllers/inventoryController');
 
 // GET all inventory items
-router.get('/inventory', inventory_controller.inventory_list);
+router.get('/', inventory_controller.inventory_list);
 
 // GET a specific inventory item by ID
-router.get('/inventory/:id', inventory_controller.inventory_detail);
+router.get('/:id', inventory_controller.inventory_detail);
 
 //POST a new inventory item
-router.post('/inventory/:id', inventory_controller.inventory_create);
+router.post('/:id', inventory_controller.inventory_create);
 
 // PUT update an exisiting inventory item by ID
-router.put('/inventory/:id', inventory_controller.inventory_update);
+router.put('/:id', inventory_controller.inventory_update);
 
 // DELETE an inventory item by ID
-router.delete('/inventory/:id', inventory_controller.inventory_delete);
+router.delete('/:id', inventory_controller.inventory_delete);
 
 module.exports = router;
