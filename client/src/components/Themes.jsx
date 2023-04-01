ae95bccc351ce5ed280efadd2b4091890572ae72
Michael Guedko <mjgeko@gmail.com>
2023-04-01 15:42:35 -0400

    Add themes

---
+++ client/src/components/Themes.jsx
@@ -0,0 +1,16 @@
+import { createTheme } from '@mui/material/styles';
+
+export const theme = createTheme({
+    palette: {
+      mode: 'dark',
+      primary: {
+        main: '#3349c4',
+      },
+      secondary: {
+        main: '#ae33c4',
+      },
+      error: {
+        main: '#c43349',
+      },
+    },
+  });
