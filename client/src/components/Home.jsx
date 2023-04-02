98d76ca769b0c0b3adeda9f37ac62c4a60576236
Michael Guedko <mjgeko@gmail.com>
2023-04-02 12:58:46 -0400

    Add some headers

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,14 @@
+import { Box, Paper, Typography } from "@mui/material";
 
 
 export default function Home() {
 
 
     return(
-        <></>
+        <Box sx={{display: 'flex', flexDirection: 'column', margin: '1rem', width: 'calc(100% - 2rem)', height: 'calc(100% - 2rem - 64px)'}}>
+           <Paper elevation={2}>
+            <Typography variant="h4">Home</Typography>
+           </Paper>
+        </Box>
     )
 }
