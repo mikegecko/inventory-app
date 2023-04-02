a1cc270661ba1cea5988f2e54ce36b2bb9a37dc0
Michael Guedko <mjgeko@gmail.com>
2023-04-02 15:51:43 -0400

    Design add item form

--- client/src/components/CreateItemForm.jsx
+++ client/src/components/CreateItemForm.jsx
@@ -1,4 +1,4 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from "@mui/material";
 import axios from "axios";
 
 export default function CreateItemForm(props) {
@@ -7,12 +7,21 @@ export default function CreateItemForm(props) {
     return(
         <Dialog open={props.open} onClose={props.handleClose}>
             <DialogTitle>
-                Create a new item
+                Add new item
             </DialogTitle>
-            <DialogContent>
+            <DialogContent sx={{margin: '.5rem', padding: '.5rem', display: 'flex', flexDirection: 'column', gap: '1rem' }}>
                 <DialogContentText>
-                    Test content
+                    
                 </DialogContentText>
+                <Box>
+                <TextField required label="Item Name" sx={{mr: 2}}/>
+                <TextField label="Category"/>
+                </Box>
+                <TextField label="Description"/>
+                <Box>
+                <TextField required label="Price" sx={{mr: 2}}/>
+                <TextField required label="Quantity" type="number"/>
+                </Box>
             </DialogContent>
             <DialogActions>
                 <Button onClick={props.handleClose}>Cancel</Button>
