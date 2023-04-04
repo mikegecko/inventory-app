968b003035db22cf0fa9b9aedcfd669a582f5808
Michael Guedko <mjgeko@gmail.com>
2023-04-04 15:57:35 -0400

    Add state for holding current selection

--- client/src/components/InventoryDashboard.jsx
+++ client/src/components/InventoryDashboard.jsx
@@ -5,8 +5,13 @@ import AddIcon from '@mui/icons-material/Add';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import CreateItemForm from "./CreateItemForm";
+import DeleteItemForm from "./DeleteItemForm";
+import { useEffect, useState } from "react";
 
 export default function InventoryDashboard(props){
+
+    const [selection, setSelection] = useState([]);
+    const [selectedItem, setSelectedItem] = useState();
 
     const modifiedInventoryItems = (arr) => {
         const newItems = [...arr] || [...props.inventoryItems];
@@ -29,20 +34,31 @@ export default function InventoryDashboard(props){
         
     ]
 
+    useEffect(() => {
+        console.log(selection);
+        if(selection !== null){
+            setSelectedItem(props.inventoryItems[selection]);
+        }
+    }, [selection])
+    useEffect(() => {
+        console.log(selectedItem)
+    }, [selectedItem])
+    
     return(
         <>
         <CreateItemForm handleClose={props.handleClose} handleClickOpenModal={props.handleClickOpenModal} open={props.openNew} handleNewItemSubmit={props.handleNewItemSubmit} />
+        <DeleteItemForm handleClose={props.handleClose} handleClickOpenModal={props.handleClickOpenModal} open={props.openDelete} selectedItem={selectedItem} />
         <Box sx={{width: 'calc(100% - 40px)', maxWidth: '100%', maxHeight:'100vh', height: 'calc(100% - 157px)', margin: ' 0 20px 20px 20px'}}>
             <Box sx={{display: 'flex', justifyContent: 'center', mb: '10px', mt: '10px'}}>
                 <Paper elevation={2} sx={{padding: '.5rem', width: 'auto'}}>
                     <ButtonGroup color="primary" sx={{ width: 'auto'}}>
                         <Button id="add" onClick={props.handleClickOpenModal}><AddIcon sx={{pointerEvents: 'none'}} /></Button>
                         <Button id="edit"><EditIcon sx={{pointerEvents: 'none'}} /></Button>
-                        <Button id="del" ><DeleteIcon sx={{pointerEvents: 'none'}} /></Button>
+                        <Button id="del" onClick={props.handleClickOpenModal} ><DeleteIcon sx={{pointerEvents: 'none'}} /></Button>
                     </ButtonGroup>
                 </Paper>
             </Box>
-            <DataGrid  columns={columns} rows={modifiedInventoryItems(props.inventoryItems)} />
+            <DataGrid rowSelectionModel={selection} onRowSelectionModelChange={(newSelection) => setSelection(newSelection)} columns={columns} rows={modifiedInventoryItems(props.inventoryItems)} />
         </Box>
         </>
     )
