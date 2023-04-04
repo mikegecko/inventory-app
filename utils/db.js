920c02fe84371afe141b088e9cd53a414a86b0b9
Michael Guedko <mjgeko@gmail.com>
2023-04-04 16:25:09 -0400

    Change connection string to env

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.set("strictQuery", false);
 
-const mongoDB =`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_KEY}@cluster0.ftvxotu.mongodb.net/?retryWrites=true&w=majority`
+const mongoDB = process.env.MONGODB_CONNECTION_STRING;
 
 main().catch((err) => console.log(err));
 async function main() {
