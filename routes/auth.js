e78cea3362cfb5399f9ddf3ad0c7514728582068
Michael Guedko <mjgeko@gmail.com>
2023-04-10 17:34:11 -0400

    Add auth route

---
+++ routes/auth.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+//Controller
+const auth_controller = require('../controllers/authController');
+
+//Api Endpoints
+
+//POST Login
+router.post();
+
+//POST Logout
+router.post();
+
+//PUT Update password
+router.put();
+
+module.exports = router;
