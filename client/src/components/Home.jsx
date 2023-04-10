1d81012e50499f72d933e4bf4892d8c6e5c39776
Michael Guedko <mjgeko@gmail.com>
2023-04-10 14:35:04 -0400

    Add mobile view

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -8,127 +8,256 @@ import {
 } from "@mui/material";
 
 export default function Home(props) {
+  const totalInventoryValue = () => {
+    const items = props.inventoryItems;
+    let total = 0;
+    items.forEach((item) => {
+      total = total + parseInt(item.quantity) * parseFloat(item.price);
+    });
+    total = total.toLocaleString("en-US", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    });
+    return total;
+  };
+  const totalInventoryItems = () => {
+    const items = props.inventoryItems;
+    let total = 0;
+    items.forEach((item) => {
+      total = total + parseInt(item.quantity);
+    });
+    return total;
+  };
+  const lowInventoryStock = () => {
+    const items = props.inventoryItems;
+    let lowItems = [];
+    items.forEach((item) => {
+      if (item.quantity <= 10 && item.quantity > 0) {
+        lowItems.push(item);
+      }
+    });
+    return lowItems;
+  };
+  const emptyInventoryStock = () => {
+    const items = props.inventoryItems;
+    let outItems = [];
+    items.forEach((item) => {
+      if (item.quantity <= 0) {
+        outItems.push(item);
+      }
+    });
+    return outItems;
+  };
 
-    const totalInventoryValue = () => {
-        const items = props.inventoryItems;
-        let total = 0;
-        items.forEach(item => {
-            total = total + (parseInt(item.quantity) * parseFloat(item.price));
-        });
-        total = total.toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 2});
-        return(total)
-    }
-    const totalInventoryItems = () => {
-        const items = props.inventoryItems;
-        let total = 0;
-        items.forEach(item => {
-            total = total + parseInt(item.quantity);
-        });
-        return(total);
-    }
-    const lowInventoryStock = () => {
-        const items = props.inventoryItems;
-        let lowItems = [];
-        items.forEach(item => {
-            if(item.quantity <= 10 && item.quantity > 0){
-                lowItems.push(item);
-            }
-        });
-        return lowItems;
-    }
-    const emptyInventoryStock = () => {
-        const items = props.inventoryItems;
-        let outItems = [];
-        items.forEach(item => {
-            if(item.quantity <= 0){
-                outItems.push(item);
-            }
-        });
-        return(outItems);
-    }
-
-
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        margin: "1rem",
-        gap: "1rem",
-        minWidth: '955px',
-        width: "calc(100% - 2rem)",
-        height: "calc(100% - 2rem - 64px)",
-      }}
-    >
-      <Paper elevation={2}>
-        <Typography variant="h4">Home</Typography>
-      </Paper>
-      <Box sx={{ width: '100%', display: 'flex', gap: '1rem', alignItems: 'flex-start'}}>
-        <Card sx={{ minWidth: "275px", width: 'auto', flex: '1 1 auto' }}>
-          <CardContent>
-            <Typography
-              sx={{ fontSize: "14px" }}
-              gutterBottom
-              color="text.secondary"
-            >
-              Total Items
-            </Typography>
-            <Typography variant="h5">{totalInventoryItems()}</Typography>
-          </CardContent>
-          <CardActionArea></CardActionArea>
-        </Card>
-        <Card sx={{ minWidth: "275px", width: 'auto', flex: '1 1 auto' }}>
-          <CardContent>
-            <Typography
-              sx={{ fontSize: "14px" }}
-              gutterBottom
-              color="text.secondary"
-            >
-              Total Value
-            </Typography>
-            <Typography variant="h5">${totalInventoryValue()}</Typography>
-          </CardContent>
-          <CardActionArea></CardActionArea>
-        </Card>
-        
-      </Box>
-      <Box sx={{ width: '100%', display: 'flex', gap: '1rem', alignItems: 'flex-start'}}>
-
-      <Card sx={{ minWidth: "275px", width: 'auto', flex: '1 1 auto'  }}>
-          <CardContent>
-            <Typography
-              sx={{ fontSize: "14px" }}
-              gutterBottom
-              color="text.secondary"
-            >
-              Low Stock Items
-            </Typography>
-            <Typography variant="ul">{lowInventoryStock().map((item,index) => {
-                return(
-                    <li key={index}>{item.title} - {item.quantity}</li>
-                )
-            })}</Typography>
-          </CardContent>
-          <CardActionArea></CardActionArea>
-        </Card>
-        <Card sx={{ minWidth: "275px", width: 'auto', flex: '1 1 auto'  }}>
-          <CardContent>
-            <Typography
-              sx={{ fontSize: "14px" }}
-              gutterBottom
-              color="text.secondary"
-            >
-              Out of Stock Items
-            </Typography>
-            <Typography variant="ul">{emptyInventoryStock().map((item,index) => {
-                return(
-                    <li key={index}>{item.title} - {item.quantity}</li>
-                )
-            })}</Typography>
-          </CardContent>
-          <CardActionArea></CardActionArea>
-        </Card>
+  if (props.mobileView) {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: '1rem',
+        }}
+      >
+        <Paper elevation={2}>
+          <Typography variant="h4">Home</Typography>
+        </Paper>
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            gap: "1rem",
+            alignItems: "flex-start",
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          <Card sx={{ minWidth: "275px", width: "auto", flex: "1 1 auto" }}>
+            <CardContent>
+              <Typography
+                sx={{ fontSize: "14px" }}
+                gutterBottom
+                color="text.secondary"
+              >
+                Total Items
+              </Typography>
+              <Typography variant="h5">{totalInventoryItems()}</Typography>
+            </CardContent>
+            <CardActionArea></CardActionArea>
+          </Card>
+          <Card sx={{ minWidth: "275px", width: "auto", flex: "1 1 auto" }}>
+            <CardContent>
+              <Typography
+                sx={{ fontSize: "14px" }}
+                gutterBottom
+                color="text.secondary"
+              >
+                Total Value
+              </Typography>
+              <Typography variant="h5">${totalInventoryValue()}</Typography>
+            </CardContent>
+            <CardActionArea></CardActionArea>
+          </Card>
         </Box>
-    </Box>
-  );
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            gap: "1rem",
+            alignItems: "flex-start",
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}
+        >
+          <Card sx={{ minWidth: "275px", width: "auto", flex: "1 1 auto" }}>
+            <CardContent>
+              <Typography
+                sx={{ fontSize: "14px" }}
+                gutterBottom
+                color="text.secondary"
+              >
+                Low Stock Items
+              </Typography>
+              <Typography variant="ul">
+                {lowInventoryStock().map((item, index) => {
+                  return (
+                    <li key={index}>
+                      {item.title} - {item.quantity}
+                    </li>
+                  );
+                })}
+              </Typography>
+            </CardContent>
+            <CardActionArea></CardActionArea>
+          </Card>
+          <Card sx={{ minWidth: "275px", width: "auto", flex: "1 1 auto" }}>
+            <CardContent>
+              <Typography
+                sx={{ fontSize: "14px" }}
+                gutterBottom
+                color="text.secondary"
+              >
+                Out of Stock Items
+              </Typography>
+              <Typography variant="ul">
+                {emptyInventoryStock().map((item, index) => {
+                  return (
+                    <li key={index}>
+                      {item.title} - {item.quantity}
+                    </li>
+                  );
+                })}
+              </Typography>
+            </CardContent>
+            <CardActionArea></CardActionArea>
+          </Card>
+        </Box>
+      </Box>
+    );
+  } else {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          margin: "1rem",
+          gap: "1rem",
+          minWidth: "955px",
+          width: "calc(100% - 2rem)",
+          height: "calc(100% - 2rem - 64px)",
+        }}
+      >
+        <Paper elevation={2}>
+          <Typography variant="h4">Home</Typography>
+        </Paper>
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            gap: "1rem",
+            alignItems: "flex-start",
+          }}
+        >
+          <Card sx={{ minWidth: "275px", width: "auto", flex: "1 1 auto" }}>
+            <CardContent>
+              <Typography
+                sx={{ fontSize: "14px" }}
+                gutterBottom
+                color="text.secondary"
+              >
+                Total Items
+              </Typography>
+              <Typography variant="h5">{totalInventoryItems()}</Typography>
+            </CardContent>
+            <CardActionArea></CardActionArea>
+          </Card>
+          <Card sx={{ minWidth: "275px", width: "auto", flex: "1 1 auto" }}>
+            <CardContent>
+              <Typography
+                sx={{ fontSize: "14px" }}
+                gutterBottom
+                color="text.secondary"
+              >
+                Total Value
+              </Typography>
+              <Typography variant="h5">${totalInventoryValue()}</Typography>
+            </CardContent>
+            <CardActionArea></CardActionArea>
+          </Card>
+        </Box>
+        <Box
+          sx={{
+            width: "100%",
+            display: "flex",
+            gap: "1rem",
+            alignItems: "flex-start",
+          }}
+        >
+          <Card sx={{ minWidth: "275px", width: "auto", flex: "1 1 auto" }}>
+            <CardContent>
+              <Typography
+                sx={{ fontSize: "14px" }}
+                gutterBottom
+                color="text.secondary"
+              >
+                Low Stock Items
+              </Typography>
+              <Typography variant="ul">
+                {lowInventoryStock().map((item, index) => {
+                  return (
+                    <li key={index}>
+                      {item.title} - {item.quantity}
+                    </li>
+                  );
+                })}
+              </Typography>
+            </CardContent>
+            <CardActionArea></CardActionArea>
+          </Card>
+          <Card sx={{ minWidth: "275px", width: "auto", flex: "1 1 auto" }}>
+            <CardContent>
+              <Typography
+                sx={{ fontSize: "14px" }}
+                gutterBottom
+                color="text.secondary"
+              >
+                Out of Stock Items
+              </Typography>
+              <Typography variant="ul">
+                {emptyInventoryStock().map((item, index) => {
+                  return (
+                    <li key={index}>
+                      {item.title} - {item.quantity}
+                    </li>
+                  );
+                })}
+              </Typography>
+            </CardContent>
+            <CardActionArea></CardActionArea>
+          </Card>
+        </Box>
+      </Box>
+    );
+  }
 }
