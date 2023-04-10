484dff3c1b7ce79afccfde78fe2871cd258115ff
Michael Guedko <mjgeko@gmail.com>
2023-04-10 14:35:13 -0400

    Add mobile view

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,22 +1,30 @@
 import styled from "@emotion/styled";
-import { AppBar, Box, InputBase, Paper, Toolbar, Typography, alpha } from "@mui/material";
-import SearchIcon from '@mui/icons-material/Search';
-import logoUrl from '../assets/inv_clear.png';
+import {
+  AppBar,
+  Box,
+  InputBase,
+  Paper,
+  Toolbar,
+  Typography,
+  alpha,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import logoUrl from "../assets/inv_clear.png";
 export default function Header(props) {
-    const Search = styled('div')(({ theme }) => ({
-        position: 'relative',
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: alpha(theme.palette.common.white, 0.15),
-        '&:hover': {
-          backgroundColor: alpha(theme.palette.common.white, 0.25),
-        },
-        marginLeft: 0,
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-          marginLeft: theme.spacing(1),
-          width: 'auto',
-        },
-      }));
+  const Search = styled("div")(({ theme }) => ({
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+      width: "auto",
+    },
+  }));
   const SearchIconWrapper = styled("div")(({ theme }) => ({
     padding: theme.spacing(0, 2),
     height: "100%",
@@ -27,40 +35,59 @@ export default function Header(props) {
     justifyContent: "center",
   }));
   const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
+    color: "inherit",
+    "& .MuiInputBase-input": {
       padding: theme.spacing(1, 1, 1, 0),
       // vertical padding + font size from searchIcon
       paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        width: '12ch',
-        '&:focus': {
-          width: '20ch',
+      transition: theme.transitions.create("width"),
+      width: "100%",
+      [theme.breakpoints.up("sm")]: {
+        width: "12ch",
+        "&:focus": {
+          width: "20ch",
         },
       },
     },
   }));
-
-  return (
-    <Box sx={{flexGrow: 1}}>
-    <AppBar position="static">
-      <Toolbar>
-        <img className="logo" src={logoUrl} alt="Inventory Manager Logo" />
-        <Typography variant="h4" component='div' sx={{flexGrow: 1,}}></Typography>
-        <Search>
-          <SearchIconWrapper>
-            <SearchIcon />
-          </SearchIconWrapper>
-          <StyledInputBase
-              placeholder="Search…"
-              disabled={props.page !== 'inventory'}
-              inputProps={{ 'aria-label': 'search' }}
-            />
-        </Search>
-      </Toolbar>
-    </AppBar>
-    </Box>
-  );
+  if (props.mobileView) {
+    return (
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography
+              variant="h4"
+              component="div"
+              sx={{ flexGrow: 1 }}
+            ></Typography>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    );
+  } else {
+    return (
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <img className="logo" src={logoUrl} alt="Inventory Manager Logo" />
+            <Typography
+              variant="h4"
+              component="div"
+              sx={{ flexGrow: 1 }}
+            ></Typography>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search…"
+                disabled={props.page !== "inventory"}
+                inputProps={{ "aria-label": "search" }}
+              />
+            </Search>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    );
+  }
 }
