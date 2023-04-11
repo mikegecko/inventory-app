7a243a28e63a937e81d669bef65f0cc6f8571f43
Michael Guedko <mjgeko@gmail.com>
2023-04-11 14:38:03 -0400

    Export function

--- utils/db.js
+++ utils/db.js
@@ -5,8 +5,10 @@ mongoose.set("strictQuery", false);
 const mongoDB = process.env.MONGODB_CONNECTION_STRING;
 
 db_connect().catch((err) => console.log(err));
-async function main() {
+async function db_connect() {
   await mongoose.connect(mongoDB, {
     bufferCommands: false, //Allows larger payloads
   });
 }
+
+module.exports = { db_connect }
